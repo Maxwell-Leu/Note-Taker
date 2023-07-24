5e217ec6d68b57095ed1a88c7ce51829743718fd
Maxwell L <mleuthner2002@gmail.com>
2023-07-24 18:30:56 -0500

    changed port

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const path = require('path');
 const api = require('./routes/index');
-const PORT = 5500;
+const PORT = 80;
 const app = express();
 
 app.use(express.json());
@@ -19,5 +19,5 @@ app.get('/notes', (req , res) =>
 );
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at port: ${PORT}`)
 );
