9247154dc2fe3a8af0074231576eb2a754d3540c
Maxwell L <mleuthner2002@gmail.com>
2023-07-24 13:11:46 -0500

    button switches pages

--- Server.js
+++ Server.js
@@ -1,20 +1,25 @@
 const express =require('express');
-
 const path = require('path');
-
+const notedata = require('./db/db.json');
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 5500;
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "./public")))
 
 app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req , res) => 
-    res.sendFile(path.join(__dirname, 'public/notes'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => res.json(notedata));
+
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
