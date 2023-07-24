b1d4f10e125ee277228cb8f2687b890343cddc82
Maxwell L <mleuthner2002@gmail.com>
2023-07-24 14:22:04 -0500

    test note appears on the page

--- Server.js
+++ Server.js
@@ -1,8 +1,7 @@
 const express =require('express');
 const path = require('path');
-const notedata = require('./db/db.json');
 const app = express();
-
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils')
 const PORT = 5500;
 
 app.use(express.json());
@@ -17,8 +16,20 @@ app.get('/notes', (req , res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => res.json(notedata));
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request recieved for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
 
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a tip`);
+
+    const{ username, topic, tip } = req.body;
+
+    if (req.body){
+        
+    }
+})
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
