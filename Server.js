2ab077a3e61254d38e932250be431bf73c6e4ded
Maxwell L <mleuthner2002@gmail.com>
2023-07-24 19:10:19 -0500

    more heroku work

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const path = require('path');
 const api = require('./routes/index');
-const PORT = 80;
+const PORT = 8080;
 const app = express();
 
 app.use(express.json());
